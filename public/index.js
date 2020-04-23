9a131865cceeeac880af906baf45bd148b52d3f7
Matěj Bartoň <bartom47@fel.cvut.cz>
2020-04-23 13:56:12 +0200

    Added http server

--- public/index.js
+++ public/index.js
@@ -1,5 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.client();
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-type': 'text/plain'
+    });
+        res.write('Hey');
+        res.end();
+    }).listen(4000);
 
 client.login(process.env.BOT_KEY);
 client.on("ready", () => {console.log("Bot is ready.")});
