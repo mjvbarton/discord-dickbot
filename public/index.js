cbaa20b079e3519c07b2fa062f5c0371f388ac55
Matěj Bartoň <bartom47@fel.cvut.cz>
2020-04-23 13:50:52 +0200

    Patched

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.client();
 
-client.login(process.env.BOT_KEY;
+client.login(process.env.BOT_KEY);
 client.on("ready", () => {console.log("Bot is ready.")});
 client.on("message", (msg) => {
     if(msg.content === "/dick"){
