03b5c98a19a6de7abac5efede4949998a7db4c57
Matěj Bartoň <bartom47@fel.cvut.cz>
2020-04-23 13:30:04 +0200

    More dick

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ client.login("NzAyODMzNDU0MTk0MjI5MzE4.XqF1UQ.a6lkaGUnb01tAa9Bjt9rK11rfbM");
 client.on("ready", () => {console.log("Bot is ready.")});
 client.on("message", (msg) => {
     if(msg.content === "/dick"){
-        msg.reply("3========D");
+        msg.reply("3=========D");
     }
 });
