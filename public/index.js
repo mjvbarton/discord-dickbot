bec85d25e0a5b9f0d7247da1b805289984f14a29
Matěj Bartoň <bartom47@fel.cvut.cz>
2020-04-23 13:41:19 +0200

    Added env

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.client();
 
-client.login("NzAyODMzNDU0MTk0MjI5MzE4.XqF1UQ.a6lkaGUnb01tAa9Bjt9rK11rfbM");
+client.login(process.env.AUTH_TOKEN);
 client.on("ready", () => {console.log("Bot is ready.")});
 client.on("message", (msg) => {
     if(msg.content === "/dick"){
