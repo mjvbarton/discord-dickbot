8b3eb03e32736348ff1de39aea83ad7f7cd51f79
Matěj Bartoň <bartom47@fel.cvut.cz>
2020-04-23 14:12:12 +0200

    Reverted dick

--- public/index.js
+++ public/index.js
@@ -1,14 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.client();
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'text/plain'
-    });
-        res.write('Hey');
-        res.end();
-    }).listen(4000);
 
 client.login(process.env.BOT_KEY);
 client.on("ready", () => {console.log("Bot is ready.")});
